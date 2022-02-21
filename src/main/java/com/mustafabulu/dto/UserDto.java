cb361f61280fd99b2b7a406a9513eed24031619b
mustafabulu <you@example.com>
2022-02-21 17:30:45 +0300

    UserDto

---
+++ src/main/java/com/mustafabulu/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.mustafabulu.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+public class UserDto {
+
+    private Long id;
+    private Long identificationNumber;
+    private String firstName_lastName;
+    private String monthlyIncome;
+    private String phoneNumber;
+}
