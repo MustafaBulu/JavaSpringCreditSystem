24f438f9f03b5c2f4c58d0e2a695b3f3dd6fd9f3
mustafabulu <you@example.com>
2022-02-21 14:57:27 +0300

    Initial commit

---
+++ src/main/java/com/mustafabulu/JavaSpringCreditSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mustafabulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringCreditSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringCreditSystemApplication.class, args);
+    }
+
+}
