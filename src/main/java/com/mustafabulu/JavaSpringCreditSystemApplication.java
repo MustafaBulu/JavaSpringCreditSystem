3e192964a81d2662c62fe60122005d120b867801
mustafabulu <you@example.com>
2022-02-21 17:20:23 +0300

    Audit

--- src/main/java/com/mustafabulu/JavaSpringCreditSystemApplication.java
+++ src/main/java/com/mustafabulu/JavaSpringCreditSystemApplication.java
@@ -1,9 +1,17 @@
 package com.mustafabulu;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
+
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@Log4j2
 public class JavaSpringCreditSystemApplication {
 
     public static void main(String[] args) {
