2237fe038b9d48eb9a17ec4287f67d8fff47948f
mustafabulu <you@example.com>
2022-02-27 02:55:01 +0300

    getCreditScore info

--- src/main/java/com/mustafabulu/services/impl/CreditServiceImpl.java
+++ src/main/java/com/mustafabulu/services/impl/CreditServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class CreditServiceImpl implements CreditService {
 
+    //kredi skorunu getiren servis daha önceden yazıldığı varsayıldığı için bu kısımda el ile kontrol edilmesi için bu şekilde oluşturulmuştur.
+
     @Override
     public int getCreditScore(long identificationNumber) {
         return 600;
